175316a67096de96e2b2407cb5fed3701b6d18de
anam81 <121813494+anam81@users.noreply.github.com>
2026-03-30 22:47:29 +0200

    Add files via upload

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,50 +22,46 @@ window.addEventListener("pagehide", () => {
 });
 
 // Scroll-Logik + Load
-window.addEventListener("DOMContentLoaded", () => {
-    const wrapper = document.querySelector(".video-wrapper");
-    const fromGrid = sessionStorage.getItem("fromGrid");
-    const savedScrollY = sessionStorage.getItem("scrollY");
+const wrapper = document.querySelector(".video-wrapper");
+const fromGrid = sessionStorage.getItem("fromGrid");
+const savedScrollY = sessionStorage.getItem("scrollY");
 
-    if (!fromGrid) {
-        window.scrollTo(0, 0);
+if (!fromGrid) {
+    window.scrollTo(0, 0);
+}
+
+if (savedScrollY !== null && fromGrid) {
+    window.scrollTo(0, parseInt(savedScrollY, 10));
+}
+
+if (wrapper && fromGrid) {
+    const rect = wrapper.getBoundingClientRect();
+
+    if (rect.top < 0 || rect.bottom > window.innerHeight) {
+        window.scrollTo({
+            top: rect.top + window.pageYOffset - 20,
+            behavior: "smooth",
+        });
     }
+}
 
-    if (savedScrollY !== null && fromGrid) {
-        window.scrollTo(0, parseInt(savedScrollY, 10));
+document.body.classList.add("is-loaded");
+
+// Ratio
+if (wrapper) {
+    const ratio = wrapper.dataset.ratio;
+
+    wrapper.classList.remove(
+        "video-16-10",
+        "video-4-3",
+        "video-1-1",
+        "video-235-100",
+    );
+
+    if (ratio && ratio !== "16/9") {
+        wrapper.classList.add("video-" + ratio.replace("/", "-"));
     }
-
-    requestAnimationFrame(() => {
-        if (wrapper && fromGrid) {
-            const rect = wrapper.getBoundingClientRect();
-
-            if (rect.top < 0 || rect.bottom > window.innerHeight) {
-                window.scrollTo({
-                    top: rect.top + window.pageYOffset - 20,
-                    behavior: "smooth",
-                });
-            }
-        }
-
-        document.body.classList.add("is-loaded");
-    });
-
-    // Ratio
-    if (wrapper) {
-        const ratio = wrapper.dataset.ratio;
-
-        wrapper.classList.remove(
-            "video-16-10",
-            "video-4-3",
-            "video-1-1",
-            "video-235-100",
-        );
-
-        if (ratio && ratio !== "16/9") {
-            wrapper.classList.add("video-" + ratio.replace("/", "-"));
-        }
-    }
-});
+}
 
 window.addEventListener("load", () => {
     sessionStorage.removeItem("fromGrid");
