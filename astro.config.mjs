1d6b1577312f0d8a379531a3fa69f62736159c09
anam81 <121813494+anam81@users.noreply.github.com>
2026-04-07 13:36:13 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
     site: 'https://arnemuench.de',
     integrations: [sitemap()],
     compressHTML: false,
+    trailingSlash: 'never',
     vite: {
         define: {
             BUILD_TIME: JSON.stringify(Date.now())
