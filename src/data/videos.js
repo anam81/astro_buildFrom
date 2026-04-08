93a856be0b49fdd043eced4d591ab9699b7ccd02
anam81 <121813494+anam81@users.noreply.github.com>
2026-04-08 18:22:57 +0200

    Add files via upload

---
+++ src/data/videos.js
@@ -0,0 +1,124 @@
+export const videos = [
+  { 
+    id: "43720180", 
+    title: "demoreel", 
+    description: {
+      en:
+        'A showcase of my work.<br>' +
+        'Browse my personal work below, visit my ' +
+        '<a href="/en/motion-graphics-clients/" style="white-space: nowrap;">client projects page</a> ' +
+        'or learn more about my work as a ' +
+        '<a href="/en/motion-design-koeln/" style="white-space: nowrap;">motion designer in Cologne</a>.',
+      de:
+        'Hier ist mein Demoreel.<br>' +
+        'Schaue unten meine persönlichen Projekte an, besuche meine ' +
+        '<a href="/de/motion-graphics-clients/" style="white-space: nowrap;">Seite mit Kundenprojekten</a> ' +
+        'oder erfahre mehr über meine Arbeit als ' +
+        '<a href="/de/motion-design-koeln/" style="white-space: nowrap;">Motion Designer in Köln</a>.'
+    }
+  },
+  {
+    id: "1174569556",
+    title: "walk sessions",
+    description: {
+      en: "It was a lot of fun making these funky little characters walk and generate the beats—sound on!",
+      de: "Es hat richtig Spaß gemacht, diese kleinen funkigen Figuren laufen zu lassen und die Beats zu produzieren — unbedingt Ton an!"
+    }
+  },
+  {
+    id: "760828515", 
+    title: "from outside",
+    description: {
+      en: "I had the honor of animating a music video for the cologne band Luciel: From Outside – " +
+          "<a href='https://www.lucielband.com' target='_blank'>www.lucielband.com</a>",
+      de: "Ich hatte das Vergnügen, ein Musikvideo für die Kölner Band Luciel zu animieren: „From Outside“ – " +
+          "<a href='https://www.lucielband.com' target='_blank'>www.lucielband.com</a>"
+    }
+  },
+  { 
+    id: "159606491", 
+    title: "impulses", 
+    description: {
+      en: "A small project I did some time ago, but I had forgotten about it. I thought it’s worth revisiting it because I like flourishing things.",
+      de: "Ein kleines vergessenes Projekt, das ich nochmal rausgekramt habe. Ich mag Wachstum und Erblühen."
+    }
+  },
+  { 
+    id: "259777046", 
+    title: "the tri drank the angle", 
+    description: {
+      en: "The tri drank the angle – exploring and playing around with triangles and hexagons",
+      de: "The tri drank the angle – ein spielerisches Experiment mit Dreiecken und Sechsecken"
+    }
+  },
+  {
+    id: "1174586225",
+    title: "amazing sight",
+    description: {
+      en: "Another character animation loop I created — really happy with how it turned out.",
+      de: "Noch eine nette Character-Loop-Animation."
+    }
+  },
+  { 
+    id: "363118107", 
+    title: "circulation", 
+    description: {
+      en: "An endless circulation",
+      de: "Ein endloser Kreislauf"
+    }
+  },
+  { 
+    id: "141259204", 
+    title: "der weg ist das ziel", 
+    description: {
+      en: 'A short animation I made that circles around the old saying "The journey is the destination". Sound by ' + 
+          "<a href='https://www.instagram.com/m.zilz/?hl=de' target='_blank'>Marcus Zilz</a>",
+      de: 'Eine kleine Animation, die sich um das alte Sprichwort "Der Weg ist das Ziel" dreht. Sound von ' + 
+          "<a href='https://www.instagram.com/m.zilz/?hl=de' target='_blank'>Marcus Zilz</a>"
+    }
+  },
+  { 
+    id: "760855186", 
+    title: "pbm.radio", 
+    description: {
+      en: "I animated this opener for the PhotoBookMuseum Let’s Talk Sessions. Original graphics designed by " +
+          "<a href='https://jorgealderete.com/' target='_blank'>Dr. Alderete</a>",
+      de: "Ich habe diesen Opener für die PhotoBookMuseum Let’s Talk Sessions animiert. Die originalen Grafiken wurden von " +
+          "<a href='https://jorgealderete.com/' target='_blank'>Dr. Alderete</a> entworfen"
+    }
+  },
+  { 
+    id: "6798661", 
+    title: "aviarium", 
+    description: {
+      en: "Aviarium, animated short – this is a surreal, dreamlike journey through apocalyptic landscapes.",
+      de: "Aviarium, ein animierter Kurzfilm – eine surreale, traumartige Reise durch apokalyptische Landschaften."
+    }
+  },
+  { 
+    id: "13058458", 
+    title: "Floating Point", 
+    description: {
+      en: "Floating Point, experimental short - Natural phenomena such as wind, water, clouds or smoke have been recorded on video. Various techniques extract their movements, " +
+          "convert them into numbers, and write them into files. These are used to control the behaviour and the sound of artificial objects.",
+      de: "Floating Point, ein experimenteller Kurzfilm – natürliche Phänomene wie Wind, Wasser, Wolken oder Rauch wurden auf Video festgehalten. Verschiedene Techniken extrahieren ihre Bewegungen, " +
+          "wandeln sie in Zahlen um und speichern sie in Dateien. Diese werden genutzt, um das Verhalten und den Sound künstlicher Objekte zu steuern."
+    }
+  },
+  {
+    id: "115930638",
+    title: "algorhythmus",
+    description: {
+      en: "algorithm – I experimented with algorithm-driven layer duplication to form a logo.",
+      de: "algorhythmus – ich habe mit algorithmisch gesteuerten Vervielfältigungen experimentiert, die ein Logo bilden."
+    }
+  },
+  { 
+    id: "60821187", 
+    title: "ejercicio nocturno", 
+    description: {
+      en: "a kinetic type animation experiment",
+      de: "ein kinetisches Typografie Experiment"
+    }
+  }
+];
---
+++ src/data/videos_client.js
@@ -0,0 +1,199 @@
+export const clients = [
+    {
+        id: "1174945206",
+        title: "ARD one Idents",
+        description: {
+            en: "I created these vibrant TV Channel Idents for ARD ONE.",
+            de: "Farbenfrohe TV-Channel-Idents für ARD ONE"
+        },
+        textOnThumb: "ARD ONE"
+    },
+    {
+        id: "1175130607",
+        title: "Atmen: Breathe Your Life",
+        description: {
+            en: "I made the graphics for the ARD documentation 'Atmen: Breathe Your Life.' "+ 
+                "Among others I composed and animated this energetic intro/opener.",
+            de: "Grafiken für die ARD-Dokumentation „Atmen: Breathe Your Life.“ "+ 
+                "Unter anderem habe ich diesen energiegeladenen Intro-Opener gestaltet und animiert."
+        },
+        ratio: "235/100",
+        textOnThumb: "ARD Documentation"
+    },
+    {
+        id: "1175321911",
+        title: "HydroDeSal",
+        description: {
+            en: "I created this animation for HydroDeSal, a research project focused on seawater desalination in the Persian Gulf. "+ 
+                "The initiative explores decentralized systems that could provide reliable access to clean drinking water " +
+                "for communities in the region.<br>"+ 
+                "Directed by <a href='https://www.heinzenziob.de' target='_blank'>Florian Heinzen-Ziob</a><br>"+ 
+                "<a href='https://www.youtube.com/watch?v=d9KdfnNRlV4' target='_blank'>Watch the full video</a>",
+            de: "Eine Animation für HydroDeSal, ein Forschungsprojekt zur Meerwasserentsalzung im Persischen Golf. "+ 
+                "Die Initiative erforscht dezentrale Systeme, die einen zuverlässigen Zugang zu sauberem Trinkwasser " +
+                "für Gemeinden in der Region ermöglichen könnten.<br>"+ 
+                "Regie: <a href='https://www.heinzenziob.de' target='_blank'>Florian Heinzen-Ziob</a><br>"+ 
+                "<a href='https://www.youtube.com/watch?v=d9KdfnNRlV4' target='_blank'>das ganze Video ansehen</a>"
+        },
+        textOnThumb: "HydroDeSal"
+    },
+    {
+        id: "1179168211",
+        title: "Die Ehe - ZDF Magazin Royale",
+        description: {
+            en: "I created this animation for Böhmermann's ZDF Magazin Royale, satirizing the institution of marriage.",
+            de: "Ein Einspieler für Böhmermanns ZDF Magazin Royale, welcher die Institution der Ehe satirisch auseinandernimmt."
+        },
+        textOnThumb: "ZDF Magazin Royale"
+    },
+    {
+        id: "260226326",
+        title: "energy future",
+        description: {
+            en: "This was part of a pitch for the relaunch of an energy provider’s website. My role was to animate the pages, navigation, and transitions.",
+            de: "Das war Teil eines Pitches für den Relaunch der Website eines Energieanbieters. Meine Aufgabe war es, die Seiten, Navigation und die Übergänge zu animieren."
+        },
+        ratio: "16/10",
+        textOnThumb: "Page Presentation"
+    },
+    {
+        id: "221156939",
+        title: "Spotify Hashtag-Generator",
+        description: {
+            en: "I animated this video teasing Spotify´s Hashtag-Generator. Design by " +
+                "<a href='https://www.dayy.com/' target='_blank'>dayy</a>",
+            de: "Ein Teaser für den Spotify Hashtag-Generator. Design von " +
+                "<a href='https://www.dayy.com/' target='_blank'>dayy</a>"
+        },
+        textOnThumb: "Spotify x Audi"
+    },
+    {
+        id: "1174525738",
+        title: "Opener for TV Show (pitch)",
+        description: {
+            en: "A pitch we did in 2015. We designed the On-Air Packaging for a German TV Channel Show (ZDF). "+ 
+                "This is the opener.<br>"+ 
+                "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>",
+            de: "Ein Pitch aus dem Jahr 2015. Wir haben das On-Air-Design für eine ZDF-Show entwickelt. "+ 
+                "Das ist der Opener.<br>"+ 
+                "Agentur: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"
+        },
+        textOnThumb: "ZDF (Pitch)"
+    },
+    { 
+        id: "221156673", 
+        title: "Starkregen", 
+        description: {
+            en: "Graphic animation for science TV program 'Quarks & Co', WDR",
+            de: "Grafikanimation für das Wissenschafts-TV-Format 'Quarks & Co', WDR"
+        },
+        textOnThumb: "WDR Quarks"
+    },
+    {
+        id: "221081352",
+        title: "Opener ZDF Adieu",
+        description: {
+            en: "I animated this opener for the TV show ZDF Adieu.<br>"+ 
+                "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"+ 
+                "music: The Fall by Peter Rudenko licensed under CC BY 4.0",
+            de: "Der Opener für die TV-Show ZDF Adieu.<br>"+ 
+                "Agentur: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"+ 
+                "Musik: The Fall von Peter Rudenko, lizenziert unter CC BY 4.0"
+        },
+        textOnThumb: "ZDF Adieu"
+    },
+    {
+        id: "1175435060",
+        title: "ZDF Auslandsjournal",
+        description: {
+            en: "I designed and developed this graphic for ZDF Auslandsjournal: Die Doku: Kampf ums Wasser<br>"+ 
+                "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>",
+            de: "Diese Grafik habe ich für das ZDF Auslandsjournal: Die Doku: Kampf ums Wasser gestaltet und animiert.<br>"+ 
+                "Agentur: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"
+        },
+        textOnThumb: "ZDF Auslandsjournal"
+    },
+    {
+        id: "1175443814",
+        title: "WDR Cosmo, Iran im Herzen",
+        description: {
+            en: "For a WDR Cosmo podcast trailer (Iran im Herzen), I animated these graphics as an intro.<br>"+ 
+                "Agency: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>",
+            de: "Das Intro für den WDR Cosmo Podcast-Trailer (Iran im Herzen).<br>"+ 
+                "Agentur: <a href='https://www.studio-paeper.tv/' target='_blank'>Studio Paeper</a><br>"
+        },
+        textOnThumb: "WDR Cosmo"
+    },
+    { 
+        id: "222078676", 
+        title: "westart studio background", 
+        description: {
+            en: "I developed many of the screen backgrounds of the WDR Westart live studio. These are some of them.",
+            de: "Ich habe viele der Screen-Backgrounds für das WDR Westart Live-Studio entwickelt. Das hier sind einige davon."
+        },
+        textOnThumb: "WDR Westart"
+    },
+    { 
+        id: "763383592", 
+        title: "Gesundes Sperma", 
+        description: {
+            en: "Compositing and animation for science TV program Quarks, WDR",
+            de: "Compositing und Animation für das Wissenschafts-TV-Format Quarks, WDR"
+        },
+        textOnThumb: "WDR Quarks"
+    },
+    { 
+        id: "760875033", 
+        title: "Warum werden Tiere im Zoo getötet", 
+        description: {
+            en: "Animation for science tv program Quarks, WDR",
+            de: "Animation für das Wissenschafts-TV-Format Quarks, WDR"
+        },
+        textOnThumb: "WDR Quarks"
+    },
+    { 
+        id: "760873301", 
+        title: "7Dinge_Stimme", 
+        description: {
+            en: "Animation for science tv program Quarks, WDR",
+            de: "Animation für das Wissenschafts-TV-Format Quarks, WDR"
+        },
+        textOnThumb: "WDR Quarks"
+    },
+    { 
+        id: "260259258", 
+        title: "multiresistente Keime", 
+        description: {
+            en: "Animation for science tv program Quarks, WDR",
+            de: "Animation für das Wissenschafts-TV-Format Quarks, WDR"
+        },
+        textOnThumb: "WDR Quarks"
+    },
+    { 
+        id: "90454734", 
+        title: "ARD Radio Feature", 
+        description: {
+            en: 'I animated this TV trailer for „Das ARD Radio Feature“',
+            de: 'TV-Trailer für „Das ARD Radio Feature“'
+        },
+        textOnThumb: "ARD Radio Feature"
+    },
+    {
+        id: "50453842",
+        title: "Counter IAA 2011",
+        description: {
+            en: "I designed and animated this counter for a car presentation at the IAA 2011.",
+            de: "Counter für eine Autopräsentation auf der IAA 2011"
+        },
+        textOnThumb: "IAA Presentation Counter"
+    },
+    { 
+        id: "74857016", 
+        title: "CityLeaks Documentary Titles", 
+        description: {
+            en: "Titles and graphics for CityLeaks Cologne Festival Documentary by Ömer Mutlu",
+            de: "Titel/Grafiken für die CityLeaks Köln Festival-Dokumentation von Ömer Mutlu"
+        },
+        textOnThumb: "CityLeaks"
+    }
+];
